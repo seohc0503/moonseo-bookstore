990e9c30eb12888dee3deb3daf95d59c385f53be
anonusr01 <seohc0503@gmail.com>
2026-02-25 02:41:56 +0900

    S01: health ping + error base

---
+++ src/main/java/com/moonseo/MoonseoBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.moonseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonseoBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonseoBookstoreApplication.class, args);
+	}
+
+}
